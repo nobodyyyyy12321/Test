b67563a0f4162a301407e95c4293b4bea9800a76
nobody <somebodyyyyy12321@gmail.com>
2026-03-19 08:45:08 +0800

    建構中

---
+++ app/account/plan/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function PlanPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[60vh]">
+      <h1 className="text-2xl font-bold mb-4">付費方案</h1>
+      <div className="text-lg text-zinc-500">建構中，敬請期待！</div>
+    </div>
+  );
+}
