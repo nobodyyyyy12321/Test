13f676644203f9379b51455b1f43de45e7a82365
nobody <somebodyyyyy12321@gmail.com>
2026-03-25 14:11:23 +0800

    登入修復

--- app/components/ShowLanguageSelectorOnHome.tsx
+++ app/components/ShowLanguageSelectorOnHome.tsx
@@ -6,7 +6,10 @@ export default function ShowLanguageSelectorOnHome() {
   const pathname = usePathname();
   if (pathname !== "/") return null;
   return (
-    <div className="flex-shrink-0 flex items-center gap-6 pr-5">
+    <div
+      className="flex-shrink-0 flex items-center gap-6 pr-5"
+      style={{ marginRight: '2.5rem', marginTop: '1.5rem' }}
+    >
       <LanguageSelector />
     </div>
   );
