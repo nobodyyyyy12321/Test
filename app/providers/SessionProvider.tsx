3acda23c29c53eef65b18a87e884dbcd77de72d3
nobody <somebodyyyyy12321@gmail.com>
2026-04-06 17:42:59 +0800

    抓session顯示登入狀態

--- app/providers/SessionProvider.tsx
+++ app/providers/SessionProvider.tsx
@@ -4,5 +4,5 @@ import React from "react";
 import { SessionProvider } from "next-auth/react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider refetchOnWindowFocus={true} refetchWhenOffline={false}>{children}</SessionProvider>;
 }
