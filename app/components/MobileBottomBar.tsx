5ff44ecfffc8d672e9cbb2b3c1cf0262675e04e1
nobody <somebodyyyyy12321@gmail.com>
2026-04-24 05:35:52 +0800

    計時器

--- app/components/MobileBottomBar.tsx
+++ app/components/MobileBottomBar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import AuthNav from "./AuthNav";
 import ShareButton from "./ShareButton";
 import UploadButton from "./UploadButton";
+import TimerButton from "./TimerButton";
 
 export default function MobileBottomBar() {
 
@@ -24,6 +25,9 @@ export default function MobileBottomBar() {
       </div>
       <div className="flex items-center justify-center w-10 h-10 rounded-full transition-colors hover:bg-zinc-200 dark:hover:bg-zinc-700">
         <UploadButton />
+      </div>
+      <div className="flex items-center justify-center w-10 h-10 rounded-full transition-colors hover:bg-zinc-200 dark:hover:bg-zinc-700">
+        <TimerButton placement="top" />
       </div>
       <div className="flex items-center justify-center w-10 h-10 rounded-full transition-colors hover:bg-zinc-200 dark:hover:bg-zinc-700">
         <AuthNav />
