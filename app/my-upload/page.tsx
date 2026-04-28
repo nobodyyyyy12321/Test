b75d5267975e95daacee8cfce494b376aaac4a7f
nobody <somebodyyyyy12321@gmail.com>
2026-04-28 15:55:16 +0800

    刪除不用的檔案

--- app/my-upload/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-import { auth } from "@/auth";
-import MyUploadClient from "./MyUploadClient";
-
-export default async function MyUploadPage() {
-  const session = await auth();
-  if (!session?.user) redirect("/auth/login");
-  return <MyUploadClient />;
-}
